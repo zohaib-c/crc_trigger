dc6f7975405aba27738e498a995f132f4e2bfc85
Zohaib Zaheer <zohaibzaheer99@gmail.com>
2024-01-11 20:24:54 +0400

    Testing gh action 2

--- function_app.py
+++ function_app.py
@@ -10,11 +10,13 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     table_conn_str = os.environ["TABLE_CONN_STR"]
+    
     table_client = TableClient.from_connection_string(conn_str=table_conn_str, table_name="visitorCount")
     entity = table_client.get_entity(partition_key="VisitorCount", row_key="CurrentCount")
 
     count = entity["count"] + 1
     entity["count"] = count
+
     table_client.update_entity(mode="merge", entity=entity)
 
     return func.HttpResponse(f"{count}", status_code=200)
